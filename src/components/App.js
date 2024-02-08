e38d4d45486ab1fea0cd2fdc91ea5cfe866aa35a
decobang <davidcoggin123@gmail.com>
2024-02-08 04:32:13 -0600

    new project using leaflet map

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import styles from '../styles/App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.appContainer}>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
